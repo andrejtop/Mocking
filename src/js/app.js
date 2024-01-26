f39c345b973e7a818a3faa6d401a40f25e126279
AndreyToptaev <Bonafide111@yandex.ru>
2024-01-26 15:28:24 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 export function healthIndicator(object) {
-    if (object.health > 50) {
-      return 'healthy';
-    } if (object.health > 15) {
-      return 'wounded';
-    }
-    return 'critical';
+  if (object.health > 50) {
+    return 'healthy';
+  } if (object.health > 15) {
+    return 'wounded';
   }
-  
-  export function sortCharacters(array) {
-    return array.sort((x, y) => y.health - x.health);
-  }
+  return 'critical';
+}
+
+export function sortCharacters(array) {
+  return array.sort((x, y) => y.health - x.health);
+}
